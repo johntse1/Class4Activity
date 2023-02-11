4588d2cda551b109de4072bd9f2ad952975001b3
John Tse <jtse63@gmail.com>
2023-02-10 19:35:28 -0500

    initial commit
    namespace zoo with interface animal and lion class

---
+++ Class4.cs
@@ -0,0 +1,41 @@
+﻿using System:
+
+namespace zoo{
+    interface Animal
+    {
+        //Interface for all animals
+        private string name;
+        private string description;
+        private int quantity;
+
+        //has both setter and getter
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public string description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public int quantity
+        {
+            get { return quantity; }
+            set { quanitty = value; }
+        }
+    }
+    class lion : Animal
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lion l1 = new lion();
+
+        }
+    }
+}
